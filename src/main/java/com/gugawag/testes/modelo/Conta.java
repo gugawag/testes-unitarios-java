acd2d3c6b17e736ff45002a6525a5ef46e0c88b2
Gustavo Wagner <gugawag@gmail.com>
2022-06-28 22:37:34 -0300

    v3-testes de Conta

---
+++ src/main/java/com/gugawag/testes/modelo/Conta.java
@@ -0,0 +1,41 @@
+package com.gugawag.testes.modelo;
+
+public class Conta {
+
+    public Conta() {
+        this.saldo = 0.0;
+    }
+
+    private String numero;
+//    private Cliente cliente;
+    private Double saldo;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void creditar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+        }
+    }
+
+    public void debitar(double valor) throws SaldoNegativoInvalidoException {
+
+        if (valor > this.saldo) {
+            throw new SaldoNegativoInvalidoException();
+        }
+
+        if (valor > 0) {
+            this.saldo -= valor;
+        }
+    }
+}
